0f711276615df3eed5800691a911714880aa6c31
Alex Chura <churaalex56@gmail.com>
2023-06-20 11:36:16 -0400

    Agregar archivos de proyecto.

---
+++ 2DOPARCIAL/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace _2DOPARCIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbCerrar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtMat = new System.Windows.Forms.TextBox();
+            this.txtSigla = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCerrar
+            // 
+            this.pbCerrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbCerrar.BackgroundImage")));
+            this.pbCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCerrar.Location = new System.Drawing.Point(516, 34);
+            this.pbCerrar.Name = "pbCerrar";
+            this.pbCerrar.Size = new System.Drawing.Size(63, 67);
+            this.pbCerrar.TabIndex = 0;
+            this.pbCerrar.TabStop = false;
+            this.pbCerrar.Click += new System.EventHandler(this.pbCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(143, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registro de notas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(247, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(240, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Materia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(266, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Sigla";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(38, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Valor a buscar";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(223, 562);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(176, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LUIS ALEX CHURA QUISPE";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.Yellow;
+            this.btnNuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnNuevo.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.Color.Red;
+            this.btnNuevo.Location = new System.Drawing.Point(42, 119);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(143, 44);
+            this.btnNuevo.TabIndex = 7;
+            this.btnNuevo.Text = "NUEVO";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnMod
+            // 
+            this.btnMod.BackColor = System.Drawing.Color.Yellow;
+            this.btnMod.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnMod.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMod.ForeColor = System.Drawing.Color.Red;
+            this.btnMod.Location = new System.Drawing.Point(42, 179);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(143, 45);
+            this.btnMod.TabIndex = 8;
+            this.btnMod.Text = "MODIFICAR";
+            this.btnMod.UseVisualStyleBackColor = false;
+            this.btnMod.Click += new System.EventHandler(this.btnMod_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Yellow;
+            this.btnEliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnEliminar.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(42, 250);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(143, 45);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(42, 380);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.RowHeadersWidth = 51;
+            this.dgvDatos.RowTemplate.Height = 24;
+            this.dgvDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDatos.Size = new System.Drawing.Size(524, 148);
+            this.dgvDatos.TabIndex = 10;
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(358, 127);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(208, 22);
+            this.txtCod.TabIndex = 11;
+            // 
+            // txtMat
+            // 
+            this.txtMat.Location = new System.Drawing.Point(358, 187);
+            this.txtMat.Name = "txtMat";
+            this.txtMat.Size = new System.Drawing.Size(208, 22);
+            this.txtMat.TabIndex = 12;
+            // 
+            // txtSigla
+            // 
+            this.txtSigla.Location = new System.Drawing.Point(358, 258);
+            this.txtSigla.Name = "txtSigla";
+            this.txtSigla.Size = new System.Drawing.Size(208, 22);
+            this.txtSigla.TabIndex = 13;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(244, 331);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(322, 22);
+            this.txtBuscar.TabIndex = 14;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Location = new System.Drawing.Point(-3, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(613, 28);
+            this.panel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(609, 622);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtSigla);
+            this.Controls.Add(this.txtMat);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbCerrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnMod;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtMat;
+        private System.Windows.Forms.TextBox txtSigla;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 2DOPARCIAL/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _2DOPARCIAL
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Server=LAPTOP-OOV5EART\\XE;Database=BD_IDS;Trusted_Connection=True;";
+        private SqlConnection connection;
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            dataAdapter = new SqlDataAdapter("SELECT * FROM NOTAS", connection);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dgvDatos.DataSource = dataTable;
+
+            // Suscribir el evento TextChanged al método txtBuscar_TextChanged
+            txtBuscar.TextChanged += txtBuscar_TextChanged;
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            string searchTerm = txtBuscar.Text.Trim();
+            MostrarResultadosBusqueda(searchTerm);
+        }
+
+        private void MostrarResultadosBusqueda(string searchTerm)
+        {
+            DataTable dataTable = BuscarRegistros(searchTerm);
+            dgvDatos.DataSource = dataTable;
+        }
+
+        private DataTable BuscarRegistros(string searchTerm)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string selectQuery = "SELECT * FROM NOTAS WHERE materia LIKE '%' + @searchTerm + '%';";
+                SqlCommand command = new SqlCommand(selectQuery, conn);
+                command.Parameters.AddWithValue("@searchTerm", searchTerm);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                conn.Close();
+
+                return dataTable;
+            }
+        }
+
+        private void pbCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMat.Text))
+            {
+                string materia = txtMat.Text;
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string insertQuery = "INSERT INTO NOTAS (materia, sigla) VALUES (@materia, @sigla);";
+                        SqlCommand command = new SqlCommand(insertQuery, conn);
+                        command.Parameters.AddWithValue("@materia", materia);
+                        command.Parameters.AddWithValue("@sigla", materia);
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Datos insertados correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al insertar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Materia no puede estar vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnMod_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtCod.Text) && !string.IsNullOrEmpty(txtMat.Text))
+            {
+                string codigo = txtCod.Text;
+                string nuevaMateria = txtMat.Text;
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string updateQuery = "UPDATE NOTAS SET materia = @nuevaMateria, sigla = @sigla WHERE nota_nro = @codigo;";
+                        SqlCommand command = new SqlCommand(updateQuery, conn);
+                        command.Parameters.AddWithValue("@nuevaMateria", nuevaMateria);
+                        command.Parameters.AddWithValue("@sigla", nuevaMateria);
+                        command.Parameters.AddWithValue("@codigo", codigo);
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Datos actualizados correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al actualizar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Código y materia no pueden estar vacíos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtCod.Text))
+            {
+                string codigo = txtCod.Text;
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string deleteQuery = "DELETE FROM NOTAS WHERE nota_nro = @codigo;";
+                        SqlCommand command = new SqlCommand(deleteQuery, conn);
+                        command.Parameters.AddWithValue("@codigo", codigo);
+                        command.ExecuteNonQuery();
+
+                        MessageBox.Show("Registro eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al eliminar el registro: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Código no puede estar vacío.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
